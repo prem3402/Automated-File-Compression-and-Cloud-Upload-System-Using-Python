1f039f74cbabe463757ea5bdb906e4f2e8e8bddb
Prem Kumar S <premachars96@gmail.com>
2025-01-03 19:23:15 +0530

    added complete file compression logic

--- main.py
+++ main.py
@@ -1,19 +1,35 @@
 import os
 import zipfile
-import zipfile
 
 
-def compress_files(input_file="/Users/prem/Desktop/", outfile="test2.zip"):
+def compress_files(input_paths, outfile="test6.zip"):
     try:
         with zipfile.ZipFile(outfile, mode="w", compression=zipfile.ZIP_DEFLATED) as zf:
-            if os.path.isdir(input_file):
-                for root, _, files in os.walk(input_file):
-                    for file in files:
-                        full_path = os.path.join(root, file)
-                        arcname = os.path.relpath(full_path, start=input_file)
-                        zf.write(full_path, arcname=arcname)
-    except:
-        print("Error in compressing files")
+            for path in input_paths:
+                if not os.path.exists(path):
+                    print(f"Path does not exist: {path}")
+                    continue
+
+                base_folder = os.path.basename(os.path.normpath(path))
+
+                if os.path.isdir(path):
+                    for root, _, files in os.walk(path):
+                        for file in files:
+                            full_path = os.path.join(root, file)
+
+                            arcname = os.path.join(
+                                base_folder, os.path.relpath(full_path, start=path)
+                            )
+                            zf.write(full_path, arcname=arcname)
+                else:
+
+                    arcname = os.path.join(base_folder, os.path.basename(path))
+                    zf.write(path, arcname=arcname)
+
+        print(f"Files successfully compressed into {outfile}")
+    except Exception as e:
+        print(f"Error in compressing files: {e}")
 
 
-compress_files()
+input_paths = ["/Users/prem/Desktop", "/Users/prem/Downloads"]
+compress_files(input_paths)
